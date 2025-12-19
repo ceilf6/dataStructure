e9b4db77b721c379d8dcf49d7c6ff38c718751c7
WJH <3506456886@qq.com>
2025-12-19 09:53:41 +0800

    feat: 广义表

--- Chapter5-Src/gen_list/GenNode.h
+++ Chapter5-Src/gen_list/GenNode.h
@@ -3,41 +3,46 @@
 
 #ifndef __REF_GEN_LIST_NODE_TYPE__
 #define __REF_GEN_LIST_NODE_TYPE__
-enum GenListNodeType {HEAD, ATOM, LIST};
+enum GenListNodeType
+{
+	HEAD,
+	ATOM,
+	LIST
+};
 #endif
 
 // ����������
-template<class ElemType>
+template <class ElemType>
 struct GenListNode
 {
-// ���ݳ�Ա:
-	GenListNodeType tag;				
-		// ��־��,HEAD(0):ͷ���, ATOM(1):ԭ�ӽṹ, LIST(2):�����
-	GenListNode<ElemType> *tLink;	// ָ��ͬһ���е���һ�����ָ����
+	// ���ݳ�Ա:
+	GenListNodeType tag;
+	// ��־��,HEAD(0):ͷ���, ATOM(1):ԭ�ӽṹ, LIST(2):�����
+	GenListNode<ElemType> *tLink; // ָ��ͬһ���е���һ�����ָ����
 	union
 	{
-		int ref;						// tag=HEAD,ͷ���,���������
-		ElemType atom;					// tag=ATOM,���ԭ�ӽ���������
-		GenListNode<ElemType> *hLink;	// tag=LIST,���ָ���ӱ���ָ����
-	}; 
+		int ref;					  // tag=HEAD,ͷ���,���������
+		ElemType atom;				  // tag=ATOM,���ԭ�ӽ���������
+		GenListNode<ElemType> *hLink; // tag=LIST,���ָ���ӱ���ָ����
+	};
 
-// ���캯��:
+	// ���캯��:
 	GenListNode(GenListNodeType tg = HEAD, GenListNode<ElemType> *next = NULL);
-		// �ɱ�־tg��ָ��next�����������
+	// �ɱ�־tg��ָ��next�����������
 };
 
 // �����������ʵ�ֲ���
-template<class ElemType>
+template <class ElemType>
 GenListNode<ElemType>::GenListNode(GenListNodeType tg, GenListNode<ElemType> *next)
 // ����������ɱ�־tg��ָ��next�����������
 {
-	tag = tg;						// ��־
+	tag = tg; // ��־
 	if (tag == HEAD)
 		// ͷ���: ����ѭ�������Ǳ�ʾΪtLinkָ���Լ�
 		tLink = (next != NULL) ? next : this;
 	else
 		// ��ͨ���: tLink ��ʽָ��
-		tLink = next;					// ���
+		tLink = next; // ���
 }
 
 #endif
