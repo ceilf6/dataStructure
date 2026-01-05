af0bc35bd3b8c6989d70448d8e3a1dfb4b848a57
WJH <3506456886@qq.com>
2026-01-05 15:10:38 +0800

    feat: 变量参数函数的引用和非引用传递

---
+++ learn2code/函数参数传递/变量.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+// int change(int a2, int b2)
+// {
+//     a2 = 10;
+//     b2 = 20;
+//     return a2;
+// }
+
+void change(int &a2, int &b2)
+{
+    a2 = 10;
+    b2 = 20;
+}
+
+int main()
+{
+    int a = 1;
+    int b = 2;
+    change(a, b); // 引用传递仍然是变量名
+    printf("a:%d,b:%d\n", a, b);
+    // a = change(a, b);
+    // printf("a:%d,b:%d\n", a, b);
+
+    return 0;
+}
