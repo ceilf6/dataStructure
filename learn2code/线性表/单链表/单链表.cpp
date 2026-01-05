83546d9b09c62a81ca3b86b5664b1d5571927973
WJH <3506456886@qq.com>
2026-01-05 18:54:45 +0800

    feat: 顺序表

---
+++ learn2code/线性表/单链表/单链表.cpp
@@ -0,0 +1,153 @@
+#include "单链表.h"
+
+//
+template <class ElemType>
+LinkList<ElemType>::LinkList()
+{
+    head = new Node<ElemType>;
+    assert(head);
+    length = 0;
+}
+
+template <class ElemType>
+LinkList<ElemType>::LinkList(ElemType v[], int n)
+{
+    Node<ElemType> *p;
+    p = head = new Node<ElemType>;
+    assert(head);
+    for (int i = 0; i < n; i++)
+    {
+        p->next = new Node<ElemType>(v[i], NULL);
+        assert(p->next);
+        p = p->next;
+    }
+    length = n;
+}
+
+template <class ElemType>
+LinkList<ElemType>::~LinkList()
+{
+    Clear();
+    delete head;
+}
+
+template <class ElemType>
+void LinkList<ElemType>::Clear()
+{
+    Node<ElemType> *p = head->next;
+    while (p != NULL)
+    {
+        head->next = p->next;
+        delete p;
+        p = head->next;
+    }
+    length = 0;
+}
+
+template <class ElemType>
+void LinkList<ElemType>::Traverse(void (*Visit)(const ElemType &)) const
+{
+    Node<ElemType> *p = head->next;
+    while (p != NULL)
+    {
+        (*Visit)(p->data);
+        p = p->next;
+    }
+}
+
+template <class ElemType>
+int LinkList<ElemType>::LocateElem(const ElemType &e) const
+{
+    Node<ElemType> *p = head->next;
+    int count = 1;
+    while (p != NULL && p->data != e)
+    {
+        count++;
+        p = p->next;
+    }
+    return (p != NULL) ? count : 0;
+}
+
+template <class ElemType>
+LinkList<ElemType>::Status LinkList<ElemType>::GetElem(int i, ElemType &e) const
+{
+    if (i < 1 || i > length)
+        return RANGE_ERROR;
+    else
+    {
+        Node<ElemType> *p = head->next;
+        int count;
+        for (count = 1; count < i; count++)
+            p = p->next;
+        e = p->data;
+        return ENTRY_FOUND;
+    }
+}
+
+template <class ElemType>
+LinkList<ElemType>::Status LinkList<ElemType>::SetElem(int i, const ElemType &e)
+{
+    if (i < 1 || i > length)
+        return RANGE_ERROR;
+    else
+    {
+        Node<ElemType> *p = head->next;
+        int count;
+        for (count = 1; count < i; count++)
+            p = p->next;
+        p->data = e;
+        return SUCCESS;
+    }
+}
+
+template <class ElemType>
+LinkList<ElemType>::Status LinkList<ElemType>::DeleteElem(int i, ElemType &e)
+{
+    if (i < 1 || i > length)
+        return RANGE_ERROR;
+    else
+    {
+        Node<ElemType> *p = head, *q;
+        int count;
+        for (count = 1; count < i; count++)
+            p = p->next;
+        q = p->next;
+        p->next = q->next;
+        e = q->data;
+        length--;
+        delete q;
+        return SUCCESS;
+    }
+}
+
+template <class ElemType>
+LinkList<ElemType>::Status LinkList<ElemType>::InsertElem(int i, const ElemType &e)
+{
+    if (i < 1 || i > length + 1)
+        return RANGE_ERROR;
+    else
+    {
+        Node<ElemType> *p = head, *q;
+        int count;
+        for (count = 1; count < i; count++)
+            p = p->next;
+        q = new Node<ElemType>(e, p->next);
+        assert(q);
+        p->next = q;
+        length++;
+        return SUCCESS;
+    }
+}
+
+template <class ElemType>
+LinkList<ElemType>::Status LinkList<ElemType>::InsertElem(const ElemType &e)
+{
+    Node<ElemType> *p, *q;
+    q = new Node<ElemType>(e, NULL);
+    assert(q);
+    for (p = head; p->next != NULL; p = p->next)
+        ;
+    p->next = q;
+    length++;
+    return SUCCESS;
+}
---
+++ learn2code/线性表/单链表/单链表.h
@@ -0,0 +1,55 @@
+template <class ElemType>
+struct Node // 节点类
+{
+    ElemType data;
+    Node<ElemType> *next;
+
+    Node();
+    Node(ElemType e, Node<ElemType> *link = NULL);
+};
+// 节点的构造函数
+template <class ElemType>
+Node<ElemType>::Node()
+{
+    next = NULL;
+}
+template <class ElemType>
+Node<ElemType>::Node(ElemType e, Node<ElemType> *link)
+{
+    data = e;
+    next = link;
+}
+
+template <class ElemType>
+class LinkList
+{
+protected:
+    Node<ElemType> *head;
+    int length;
+
+public:
+    enum Status
+    {
+        SUCCESS,
+        FAILED,
+        RANGE_ERROR,
+        ENTRY_FOUND
+        // ...
+    }; // 别忘记分号
+
+    LinkList();
+    LinkList(ElemType v[], int n);
+    virtual ~LinkList();
+    int GetLength() const;
+    bool IsEmpty() const;
+    void Clear();
+    void Traverse(void (*Visit)(const ElemType &)) const;
+    int LocateElem(const ElemType &e) const;
+    Status GetElem(int i, ElemType &e) const;
+    Status SetElem(int i, const ElemType &e);
+    Status DeleteElem(int i, ElemType &e);
+    Status InsertElem(int i, const ElemType &e);
+    Status InsertElem(const ElemType &e);
+    LinkList(const LinkList<ElemType> &la);
+    LinkList<ElemType> &operator=(const LinkList<ElemType> &la);
+};
