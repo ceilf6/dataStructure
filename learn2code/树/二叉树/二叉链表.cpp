6280a7ca8f15b0360443bc658e43cbab589a2926
WJH <3506456886@qq.com>
2026-01-07 17:23:03 +0800

    feat: DFS三种遍历二叉树的方式

--- learn2code/树/二叉树/二叉链表.cpp
+++ learn2code/树/二叉树/二叉链表.cpp
@@ -82,3 +82,42 @@ void BinaryTree<ElemType>::
         return;
     }
 }
+
+template <class ElemType>
+void BinaryTree<ElemType>::
+    InOrder(BinTreeNode<ElemType> *r,
+            void (*Visit)(const ElemType &)) const
+{
+    if (r != NULL)
+    {
+        InOrder(r->leftChild, Visit);
+        (*Visit)(r->data);
+        InOrder(r->rightChild, Visit);
+    }
+}
+
+template <class ElemType>
+void BinaryTree<ElemType>::
+    PreOrder(BinTreeNode<ElemType> *r,
+             void (*Visit)(const ElemType &)) const
+{
+    if (r != NULL)
+    {
+        (*Visit)(r->data);
+        PreOrder(r->leftChild, Visit);
+        PreOrder(r->rightChild, Visit);
+    }
+}
+
+template <class ElemType>
+void BinaryTree<ElemType>::
+    PostOrder(BinTreeNode<ElemType> *r,
+              void (*Visit)(const ElemType &)) const
+{
+    if (r != NULL)
+    {
+        PostOrder(r->leftChild, Visit);
+        PostOrder(r->rightChild, Visit);
+        (*Visit)(r->data);
+    }
+}
