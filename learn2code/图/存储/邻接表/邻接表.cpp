726dc8a9a4547826c0be46ed2fb94a128e2b265d
WJH <3506456886@qq.com>
2026-01-08 14:04:17 +0800

    feat: 邻接表

--- learn2code/图/存储/邻接表/邻接表.cpp
+++ learn2code/图/存储/邻接表/邻接表.cpp
@@ -1,4 +1,14 @@
 #include "邻接表.h"
+template <class ElemType, class WeightType>
+struct AdjListNetWorkVex
+{
+    ElemType data;
+    AdjListNetworkArc<WeightType> *firstarc;
+
+    AdjListNetWorkVex();
+    AdjListNetWorkVex(ElemType val,
+                      AdjListNetworkArc<WeightType> *adj = NULL);
+};
 
 template <class ElemType, class WeightType>
 AdjListNetWorkVex<ElemType, WeightType>::AdjListNetWorkVex()
--- learn2code/图/存储/邻接表/邻接表.h
+++ learn2code/图/存储/邻接表/邻接表.h
@@ -1,10 +1,34 @@
+#ifndef ADJACENCY_LIST_H
+#define ADJACENCY_LIST_H
+
+#include <cstddef>
+
+template <class WeightType>
+struct AdjListNetworkArc;
+
 template <class ElemType, class WeightType>
 struct AdjListNetWorkVex
 {
     ElemType data;
     AdjListNetworkArc<WeightType> *firstarc;
 
-    AdjListNetWorkVex();
+    AdjListNetWorkVex() : firstarc(NULL) {}
     AdjListNetWorkVex(ElemType val,
-                      AdjListNetworkArc<WeightType> *adj = NULL);
+                      AdjListNetworkArc<WeightType> *adj = NULL)
+        : data(val), firstarc(adj) {}
 };
+
+template <class WeightType>
+struct AdjListNetworkArc
+{
+    int adjvex;
+    WeightType weight;
+    AdjListNetworkArc<WeightType> *next;
+
+    AdjListNetworkArc(int v,
+                      WeightType w,
+                      AdjListNetworkArc *n = NULL)
+        : adjvex(v), weight(w), next(n) {}
+};
+
+#endif
