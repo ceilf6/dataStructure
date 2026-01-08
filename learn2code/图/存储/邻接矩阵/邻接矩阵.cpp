23632d926e8639802b449165b27fb633743b8716
WJH <3506456886@qq.com>
2026-01-08 13:42:28 +0800

    feat: 邻接矩阵

--- learn2code/图/存储/邻接矩阵/邻接矩阵.cpp
+++ learn2code/图/存储/邻接矩阵/邻接矩阵.cpp
@@ -15,3 +15,42 @@ AdjMatrixUndirGraph<ElemType>::AdjMatrixUndirGraph(int vertexMaxNum)
     for (int v = 0; v < vexMaxNum; v++)
         arcs[v] = new int[vexMaxNum]; // 列数
 }
+
+// 带点信息的构造函数
+template <class ElemType>
+AdjMatrixUndirGraph<ElemType>::AdjMatrixUndirGraph(ElemType es[],
+                                                   int vertexNum, int vertexMaxNum)
+{
+    if (vertexMaxNum < 0)
+        throw Error("允许的顶点最大数目不能为负!");
+    if (vertexMaxNum < vertexNum)
+        throw Error("顶点数目不能大于允许的顶点最大数目!");
+    vexNum = vertexNum;
+    vexMaxNum = vertexMaxNum;
+    arcNum = 0;
+    vertexes = new ElemType[vexMaxNum];
+    tag = new Status[vexMaxNum];
+    arcs = (int **)new int *[vexMaxNum];
+    for (int v = 0; v < vexMaxNum; v++)
+        arcs[v] = new int[vexMaxNum];
+    for (int v = 0; v < vexNum; v++)
+    {
+        vertexes[v] = es[v];
+        tag[v] = UNVISITED;
+        for (int u = 0; u < vexNum; u++)
+            arcs[v][u] = 0;
+    }
+}
+
+// 找第一个邻接点序号
+// 在所有与目标 v 顶点直接相连的顶点中按索引顺序找到第一个相邻顶点的下标
+template <class ElemType>
+int AdjMatrixUndirGraph<ElemType>::FirstAdjVex(int v) const
+{
+    if (v < 0 || v >= vexNum)
+        throw Error("v不合法!");
+    for (int u = 0; u < vexNum; u++)
+        if (arcs[v][u] != 0)
+            return u;
+    return -1;
+}
