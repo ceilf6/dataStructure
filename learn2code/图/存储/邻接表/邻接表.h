804bdf9226d47dd20e3f5f2d1dfc0ee4da829006
WJH <3506456886@qq.com>
2026-01-08 14:08:57 +0800

    feat: 邻接表

--- learn2code/图/存储/邻接表/邻接表.h
+++ learn2code/图/存储/邻接表/邻接表.h
@@ -6,11 +6,12 @@
 template <class WeightType>
 struct AdjListNetworkArc;
 
+// 每个邻接表链表的头节点
 template <class ElemType, class WeightType>
 struct AdjListNetWorkVex
 {
-    ElemType data;
-    AdjListNetworkArc<WeightType> *firstarc;
+    ElemType data;                           // 顶点信息
+    AdjListNetworkArc<WeightType> *firstarc; // 第一条边（头节点）
 
     AdjListNetWorkVex() : firstarc(NULL) {}
     AdjListNetWorkVex(ElemType val,
@@ -18,17 +19,27 @@ struct AdjListNetWorkVex
         : data(val), firstarc(adj) {}
 };
 
+// 邻接表中每一条边的节点结构
 template <class WeightType>
 struct AdjListNetworkArc
 {
-    int adjvex;
-    WeightType weight;
-    AdjListNetworkArc<WeightType> *next;
+    int adjvex;                          // 顶点数组中的下标，标识这条边是连向哪个顶点
+    WeightType weight;                   // 边权值
+    AdjListNetworkArc<WeightType> *next; // 指向同一顶点的下一条边
 
     AdjListNetworkArc(int v,
                       WeightType w,
                       AdjListNetworkArc *n = NULL)
         : adjvex(v), weight(w), next(n) {}
 };
+
+/*
+AdjListNetWorkVex vertexes[vexNum];
+
+vertexes[0] --> arc0_1 -> arc0_2 -> arc0_3 -> NULL
+vertexes[1] --> arc1_1 -> arc1_2 -> NULL
+vertexes[2] --> NULL
+...
+*/
 
 #endif
