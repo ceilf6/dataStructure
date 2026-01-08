f588ce17060cdb8229282a27cec7501390126783
WJH <3506456886@qq.com>
2026-01-08 14:10:04 +0800

    feat: 邻接表

--- learn2code/图/存储/邻接表/邻接表.cpp
+++ learn2code/图/存储/邻接表/邻接表.cpp
@@ -1,17 +1,17 @@
 #include "邻接表.h"
-template <class ElemType, class WeightType>
-struct AdjListNetWorkVex
-{
-    ElemType data;
-    AdjListNetworkArc<WeightType> *firstarc;
 
-    AdjListNetWorkVex();
-    AdjListNetWorkVex(ElemType val,
-                      AdjListNetworkArc<WeightType> *adj = NULL);
-};
-
+// 顶点节点无参数的构造函数
 template <class ElemType, class WeightType>
 AdjListNetWorkVex<ElemType, WeightType>::AdjListNetWorkVex()
 {
     firstarc = NULL;
+}
+
+// 顶点节点有参数的构造函数
+template <class ElemType, class WeightType>
+AdjListNetWorkVex<ElemType, WeightType>::AdjListNetWorkVex(ElemType val,
+                                                           AdjListNetworkArc<WeightType> *adj)
+{
+    data = val;
+    firstarc = adj;
 }
