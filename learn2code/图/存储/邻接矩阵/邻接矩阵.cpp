fa86d27c91401e0f3e12bbd6604b7633a3fdd75f
WJH <3506456886@qq.com>
2026-01-08 13:47:17 +0800

    feat: 邻接矩阵

--- learn2code/图/存储/邻接矩阵/邻接矩阵.cpp
+++ learn2code/图/存储/邻接矩阵/邻接矩阵.cpp
@@ -87,3 +87,22 @@ void AdjMatrixUndirGraph<ElemType>::InsertVex(const ElemType &d)
     }
     vexNum++;
 }
+
+// 插入边：在邻接矩阵对应位置（无向图是对称的两处）打标记
+template <class ElemType>
+void AdjMatrixUndirGraph<ElemType>::InsertArc(int v1,
+                                              int v2)
+{
+    if (v1 < 0 || v1 >= vexNum)
+        throw Error("v1不合法!");
+    if (v2 < 0 || v2 >= vexNum)
+        throw Error("v2不合法!");
+    if (v1 == v2)
+        throw Error("v1不能等于v2!");
+    if (arcs[v1][v2] == 0)
+    {
+        arcNum++;
+        arcs[v1][v2] = 1;
+        arcs[v2][v1] = 1;
+    }
+}
